23da0876ddf5d8cb722774858c54566fe2acec15
U-BSD\419236 <419236@SHS-8YBF6X3.beaverton.k12.or.us>
2024-09-03 11:08:03 -0700

    completed august 3rd, 2024

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -1,3 +1,15 @@
+//\
+Kojiro, Brandon \
+3 August 2024 \
+P3 Galbraith - C++/Data Structures \
+This program is a text-based number guessing game accessible via the console. The computer will choose a number between 1-100, and the player is prompted to make numerical guesses. Once the number is correctly guessed, the computer will display the number of \
+guesses made, and a play again option.
+
+// The three rules of C++ are: \
+1) No global variables \
+2) No strings (only cstrings or char arrays!) \
+3) You should include <iostream> and not stdio, as well as some other syntax things because this is C++ and not C
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -10,42 +22,37 @@ int main(){
   int numGuesses = 0;
   bool keepGuess = true;
   bool playAgain = true;
+  srand(time(NULL));
 
   while (playAgain == true){    
     //create target number
-    srand(time(NULL));
     target = rand() % 100;
+    cout << "Make your guess!\n" << "> ";
 
     //start asking
     while (keepGuess == true){
-      cout << "Make your guess!";
       numGuesses++;
-      char input[3];
-      cin.get(input, 3); //smth about this is messing up
-      int inputAsNum = atoi(input);
-      cout << inputAsNum;
-      if (inputAsNum == target){ //guess is correct
-	cout << "yay!";
+      int input = 0;
+      cin >> input;
+      if (input == target){ //guess is correct
 	keepGuess = false;
       } else { //guess is incorrect
-	if (inputAsNum > target){
-	  cout << "Too high!";
-	} else if (inputAsNum < target) {
-	  cout << "Too low!";
+	if (input > target){
+	  cout << "Too high! Try again\n" << "> ";
+	} else if (input < target) {
+	  cout << "Too low! Try again\n" << "> ";
 	}          
       }
     }
     
     //player win
-    cout << "Congratulations! You guessed the correct number";
-    cout << "It took you " << numGuesses << " guesses! Press y to play again, or press n to quit";
+    cout << "Congratulations! You guessed the correct number in " << numGuesses << " guesses!\n" << "Press y to play again, or press n to quit\n" << "> ";
     char pAInput;
     cin >> pAInput;
-    if (pAInput == 'y'){
+    if (pAInput == 'y'){ //play again
       keepGuess = true;
       numGuesses = 0;
-      
-    } else if (pAInput == 'n'){
+    } else if (pAInput == 'n'){ //quit
       playAgain = false;
     }
   }
